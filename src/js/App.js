c84712d041c73ba0770c569ea8679651ad62e6e5
desaianiruddh <aniruddh.d@simformsolutions.com>
2022-02-21 18:53:14 +0530

    eslint --fix

--- src/js/App.js
+++ src/js/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "../css/style.css";
+import React from 'react';
+import '../css/style.css';
 import natureImg from '../img/nature.jpg';
 
 const App = () => {
-	return (
-		<>
-			<h1>Hello World!!</h1>
-			<img src={natureImg} alt="nature img"></img>
-		</>
-	);
+  return (
+    <>
+      <h1>Hello World!!</h1>
+      <img src={natureImg} alt="nature img"></img>
+    </>
+  );
 };
 
 export default App;
