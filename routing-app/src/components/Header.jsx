c93a92c22f14cce7cb0c8ea2ae45b0be95a7759a
desaianiruddh <aniruddh.d@simformsolutions.com>
2022-04-07 16:56:56 +0530

    add useLocation and go back functionality

--- routing-app/src/components/Header.jsx
+++ routing-app/src/components/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <div className="container-fluid">
-        <div className="navbar-brand">Header</div>
+        <div className="navbar-brand" onClick={() => navigate('/')}>
+          Header
+        </div>
+        <div className="text-secondary">
+          You are now at{' '}
+          <span className="text-danger">"{location.pathname}"</span>
+        </div>
+        <button className="btn btn-dark" onClick={() => navigate(-1)}>
+          Go Back
+        </button>
       </div>
       <div className="navbar-nav">
         <Link className="nav-link" to="/">
